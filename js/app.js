1d2d9a2f8a63fa02d13f9e277fc47d71de9fd48c
Andranik&Gayane <kmanukyan1965@gmail.com>
2024-05-22 00:43:44 +0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -36,7 +36,7 @@ timerId = setInterval(timeCountDown, 1000);
 
 
 // const audio = new Audio("https://dl2.mp3party.net/online/8450417.mp3");
-const audio = new Audio("https://dl2.mp3party.net/online/8450417.mp3");
+const audio = new Audio("just.mp3");
 
 const buttons = document.querySelectorAll("#audio");
 audio.currentTime = 74
@@ -54,51 +54,3 @@ buttons.forEach(button => {
     }
   });
 });
-
-// const timeLeft = document.getElementById('time-left')
-// const mnacel = document.getElementById('mnacel')
-
-
-
-
-// //month day year
-// const birthday = new Date('07/29/2023')
-
-// const second = 1000
-// const minute = second * 60
-// const hour = minute * 60
-// const day = hour * 24
-// let timerId
-// console.log(second)
-// console.log(minute+ "minut")
-//  console.log(hour+"hour")
-
-// function countDown() {
-//   const today = new Date()
-//   const timeSpan = birthday - today
-
-
-//   if (timeSpan <= -day) {
-//     mnacel.innerHTML = '&#9825 &#9825 &#9825 &#9825 &#9825 &#9825 &#9825 &#9825 &#9825'
-//     timeLeft.innerHTML = '<span style="text-transform:uppercase;"> Married!! </span>'
-//     clearInterval(timerId)
-//     return
-//   }
-
-//   if (timeSpan <= 0) {
-//     mnacel.innerHTML = '&#9825 &#9825 &#9825 &#9825 &#9825 &#9825 &#9825 &#9825 &#9825 &#9825 &#9825 &#9825 &#9825'
-//     timeLeft.innerHTML = '<span style="text-transform:uppercase;"> Wedding Day!! </span>'
-//     clearInterval(timerId)
-//     return
-//   }
-
-
-//   const days = Math.floor(timeSpan / day)
-//   const hours = Math.floor(days*24)
-//   const minutes = Math.floor((timeSpan % hour) / minute)
-//   const seconds = Math.floor((timeSpan % minute) / second)
-//   timeLeft.innerHTML =    days + '<sub>օր</sub> : ' + hours + '<sub>ժ</sub> : ' + minutes + '<sub>ր</sub> : ' + seconds + '<sub>վ</sub>'
-
-// }
-
-// timerId = setInterval(countDown, second)
